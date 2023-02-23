b4280d8865182928af467f456e740cab06265a41
dhermidaperez@danielcastelao.org <dhermidaperez@danielcastelao.org>
2023-02-23 22:09:54 +0100

    Ultimos retoques de fallos

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-import InterfazCliente.Interfaz;
-
 import javax.swing.*;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Main {
     static Socket clienteSocket;
+   static int opcion = -1 ;
 
     public static void main(String[] args) throws IOException {
         conexion();
-        recibimosMensajeS();
+        enviamosMensajeS();
     }
 
     //Con este metodo creámosla con el socoket que tiene el mismo puerto que el servidor
     public static void conexion() {
         try {
-            //Sout donde comenzamos el metodo guiando todo paso a paso
+            //Println donde comenzamos el metodo guiando todo paso a paso
             System.out.println("Creando socket cliente");
             //Creamos el socket del cliente
             clienteSocket = new Socket();
@@ -35,96 +35,112 @@ public class Main {
 
     // Metodo para enviar el mensaje al servidor, al crear el metodo creamos dos variables una para el menu
     // que será un int y otra que es un String para enviar el mensaje
-    public static void enviamosMensajeS(int opcion, String mensaje) throws IOException {
+    public static void enviamosMensajeS( ) throws IOException {
 
-        try {
-            //Importante: Creamos objeto para enviar los datos, con la clase OutputStream
-            OutputStream os = clienteSocket.getOutputStream();
-            //IMPORTANTE:
-            // El objeto creado lo metemos dentro del nuevo objeto para poder enviarlo y que sea un int
-            //Y no un byte
-            DataOutputStream sos = new DataOutputStream(os);
+        while (opcion != 0) {
 
 
-            //Creamos el menu
-            switch (opcion) {
-                //Primera opcion que hace
-                case 1 -> {
-                    //Informamos de lo que vamos a hacer con un print
-                    System.out.println("Enviamos el primer mensaje ");
-                    //Le indicamos que el mensaje que le vamos a enviar es el primerp
-                    sos.writeInt(1);
-                    //Enviamos el mensaje
-                    sos.writeInt(Integer.parseInt(mensaje));
+            try {
 
-                }
+
+                //Importante: Creamos objeto para enviar los datos, con la clase OutputStream
+                OutputStream os = clienteSocket.getOutputStream();
+                //IMPORTANTE:
+                // El objeto creado lo metemos dentro del nuevo objeto para poder enviarlo y que sea un int
+                //Y no un byte
+                DataOutputStream sos = new DataOutputStream(os);
+                InputStream is = clienteSocket.getInputStream();
+                //Creamos el objeto es de la clase Data, para poder recibir más variables que el byte
+                DataInputStream eis = new DataInputStream(is);
+
+
+                String lectura = JOptionPane.showInputDialog(null,"************************" +
+                        "\n Elige opción: \n" +
+                        " 1.- CampoFutbol\n" +
+                        " 2.- Jubilacion \n" +
+                        " 3.- Arturito\n" +
+                        " 4.- gasolina \n************************");
+                opcion = Integer.parseInt(lectura);
+
+                //Creamos el menu
+                switch (opcion) {
+                    //Primera opcion que hace
+                    case 1 -> {
+                        //Pedimos la opcion nº1
+                        String uno = JOptionPane.showInputDialog("CampoFutbol");
+                        //Informamos de lo que vamos a hacer con un print
+                        System.out.println("Enviamos el primer mensaje ");
+                        //Le indicamos que el mensaje que le vamos a enviar es el primerp
+                        sos.writeInt(1);
+                        //Enviamos el mensaje
+                        sos.writeDouble(Double.parseDouble(uno));
+                        //Recibimos el mensaje de vuelta
+                        JOptionPane.showMessageDialog(null, eis.readUTF());
+
+                    }
 
                     /* APUNTE TECNICO ES UN MENSAJE POR METODO DEL SERVIDOR*/
 
-                case 2 -> {
-                    //Informamos de lo que vamos a hacer con un print
-                    System.out.println("Enviamos el segundo mensaje ");
-                    //Le indicamos que el mensaje que le vamos a enviar es el primerp
-                    sos.writeInt(2);
-                    //Enviamos el mensaje
-                    sos.writeInt(Integer.parseInt(mensaje));
+                    case 2 -> {
+                        //Pedimos la opcion nº2
+                        String dos = JOptionPane.showInputDialog("Jubilacion");
+
+                        //Informamos de lo que vamos a hacer con un print
+                        System.out.println("Enviamos el segundo mensaje ");
+                        //Le indicamos que el mensaje que le vamos a enviar es el primerp
+                        sos.writeInt(2);
+                        //Enviamos el mensaje
+                        sos.writeDouble(Double.parseDouble(dos));
+                        //Recibimos el mensaje de vuelta
+                        JOptionPane.showMessageDialog(null, eis.readUTF());
+
+                    }
+                    case 3 -> {
+                        //Pedimos la opcion nº3
+                        String tres = JOptionPane.showInputDialog("Arturito");
+                        //Informamos de lo que vamos a hacer con un print
+                        System.out.println("Enviamos el tercer mensaje ");
+                        //Le indicamos que el mensaje que le vamos a enviar es el primerp
+                        sos.writeInt(3);
+                        //Enviamos el mensaje
+                        sos.writeDouble(Double.parseDouble(tres));
+                        //Recibimos el mensaje de vuelta
+                        JOptionPane.showMessageDialog(null, eis.readUTF());
+                    }
+                    case 4 -> {
+                        //Pedimos la opcion nº4
+                        String cuatro = JOptionPane.showInputDialog("Arturito");
+                        //Informamos de lo que vamos a hacer con un print
+                        System.out.println("Enviamos el cuarto mensaje ");
+                        //Le indicamos que el mensaje que le vamos a enviar es el primerp
+                        sos.writeInt(4);
+                        //Enviamos el mensaje
+                        sos.writeDouble(Double.parseDouble(cuatro));
+                        //Recibimos el mensaje de vuelta
+                        JOptionPane.showMessageDialog(null, eis.readUTF());
+                    }
+                    case 5 -> {
+                        //Nos disponemos a cerrar el Socket lo anunciamos con un print
+                        System.out.println("Cerramos el socket cliente");
+                        //Cerramos el socket
+                        clienteSocket.close();
+                        // Metodo enviar acabado
+                        System.out.println("Acabado");
+
+                    }
+
 
                 }
-                case 3 -> {
-                    //Informamos de lo que vamos a hacer con un print
-                    System.out.println("Enviamos el tercer mensaje ");
-                    //Le indicamos que el mensaje que le vamos a enviar es el primerp
-                    sos.writeInt(3);
-                    //Enviamos el mensaje
-                    sos.writeInt(Integer.parseInt(mensaje));
-                }
-                case 4 -> {
-                    //Informamos de lo que vamos a hacer con un print
-                    System.out.println("Enviamos el cuarto mensaje ");
-                    //Le indicamos que el mensaje que le vamos a enviar es el primerp
-                    sos.writeInt(4);
-                    //Enviamos el mensaje
-                    sos.writeInt(Integer.parseInt(mensaje));
-                }
-                case 5 -> {
-                    //Nos disponemos a cerrar el Socket lo anunciamos con un print
-                    System.out.println("Cerramos el socket cliente");
-                    //Cerramos el socket
-                    clienteSocket.close();
-                    // Metodo enviar acabado
-                    System.out.println("Acabado");
 
-                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
 
 
             }
 
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-
-
-        }
-
-        //Creamos el metodo para recibir el resultado del servidor
-
-
-
-
-
-
-            }
-    public static void recibimosMensajeS() throws IOException {
-        //Delcaramos el objeto is para poder recoger el dato que nos envia el servidor, gracias a la clase InputStream
-        InputStream is = clienteSocket.getInputStream();
-        //Creamos el objeto es de la clase Data, para poder recibir más variables que el byte
-        DataInputStream eis = new DataInputStream(is);
-        while(true){
-            // recibimos el mensaje devuelto del servidor y lo enseñamos por pantalla
-            JOptionPane.showMessageDialog(null, eis.readUTF());
 
         }
 
     }
-
 }
